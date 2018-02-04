fcca389ff1d6342260d4c8e7a5323f13f3296dc2
ElPumpo <samo_kol@hotmail.com>
2018-02-04 19:20:05 +0100

    (speed-limit) Moved to ESX and fixed spam

--- client/speed_limit-client.lua
+++ client/speed_limit-client.lua
@@ -4,15 +4,26 @@
 -- Description: This script will restict the speed of the vehicle when
 --              INPUT_MP_TEXT_CHAT_TEAM is pressed. To disable, press
 --              INPUT_VEH_SUB_ASCEND + INPUT_MP_TEXT_CHAT_TEAM
+-- Modified by the SCRP team
 -------------------------------------------------------------------------------
 
+ESX = nil
+
 Citizen.CreateThread(function()
+
+
+	-- get ESX
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+	
 	local resetSpeedOnEnter = true
 	local isEnabled = false
 	while true do
 		Citizen.Wait(0)
 		local playerPed = GetPlayerPed(-1)
-		local vehicle = GetVehiclePedIsIn(playerPed,false)
+		local vehicle = GetVehiclePedIsIn(playerPed, false)
 
 		-- This should only happen on vehicle first entry to disable any old values
 		if GetPedInVehicleSeat(vehicle, -1) == playerPed and IsPedInAnyVehicle(playerPed, false) then
@@ -24,28 +35,24 @@ Citizen.CreateThread(function()
 			end
 			-- Disable speed limiter
 			if IsControlJustReleased(0,246) and IsControlPressed(0,131) then
-				if isEnabled then
-					showHelpNotification("Hastighetsbegränsare inaktiverad")
+				if isEnabled then -- fix spam
+					ESX.ShowNotification("Hastighetsbegränsare inaktiverad")
 					maxSpeed = GetVehicleHandlingFloat(vehicle,"CHandlingData","fInitialDriveMaxFlatVel")
 					SetEntityMaxSpeed(vehicle, maxSpeed)
 					isEnabled = false
 				end
 			-- Enable speed limiter
 			elseif IsControlJustReleased(0,246) then
-				isEnabled = true
-				cruise = GetEntitySpeed(vehicle)
-				SetEntityMaxSpeed(vehicle, cruise)
-				cruise = math.floor(cruise * 3.6 + 0.5)
-				showHelpNotification("Hastighetsbegränsare inställd på "..cruise.." km/h, tryck ~INPUT_VEH_SUB_ASCEND~ + ~INPUT_MP_TEXT_CHAT_TEAM~ för att inaktivera.")
+				if not isEnabled then -- fix spam
+					cruise = GetEntitySpeed(vehicle)
+					SetEntityMaxSpeed(vehicle, cruise)
+					cruise = math.floor(cruise * 3.6 + 0.5)
+					ESX.ShowNotification("Hastighetsbegränsare inställd på "..cruise.." km/h, tryck ~INPUT_VEH_SUB_ASCEND~ + ~INPUT_MP_TEXT_CHAT_TEAM~ för att inaktivera.")
+					isEnabled = true
+				end
 			end
 		else
 			resetSpeedOnEnter = true
 		end
 	end
 end)
-
-function showHelpNotification(msg)
-	BeginTextCommandDisplayHelp("STRING")
-	AddTextComponentSubstringPlayerName(msg)
-	EndTextCommandDisplayHelp(0, 0, 1, -1)
-end
