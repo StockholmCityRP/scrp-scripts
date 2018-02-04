17a95c47292b2b90a4bbe076e379a94acfd2ce31
ElPumpo <samo_kol@hotmail.com>
2018-02-04 18:06:46 +0100

    Fixed spacing (it was awful)

--- client/speed_limit-client.lua
+++ client/speed_limit-client.lua
@@ -7,40 +7,39 @@
 -------------------------------------------------------------------------------
 
 Citizen.CreateThread(function()
-  local resetSpeedOnEnter = true
-  while true do
-    Citizen.Wait(0)
-    local playerPed = GetPlayerPed(-1)
-    local vehicle = GetVehiclePedIsIn(playerPed,false)
-    if GetPedInVehicleSeat(vehicle, -1) == playerPed and IsPedInAnyVehicle(playerPed, false) then
+	local resetSpeedOnEnter = true
+	while true do
+		Citizen.Wait(0)
+		local playerPed = GetPlayerPed(-1)
+		local vehicle = GetVehiclePedIsIn(playerPed,false)
 
-      -- This should only happen on vehicle first entry to disable any old values
-      if resetSpeedOnEnter then
-        maxSpeed = GetVehicleHandlingFloat(vehicle,"CHandlingData","fInitialDriveMaxFlatVel")
-        SetEntityMaxSpeed(vehicle, maxSpeed)
-        resetSpeedOnEnter = false
-      end
-      -- Disable speed limiter
-      if IsControlJustReleased(0,246) and IsControlPressed(0,131) then
-        maxSpeed = GetVehicleHandlingFloat(vehicle,"CHandlingData","fInitialDriveMaxFlatVel")
-        SetEntityMaxSpeed(vehicle, maxSpeed)
-        showHelpNotification("Hastighetsbegränsare inaktiverad")
-      -- Enable speed limiter
-      elseif IsControlJustReleased(0,246) then
-        cruise = GetEntitySpeed(vehicle)
-        SetEntityMaxSpeed(vehicle, cruise)
-          cruise = math.floor(cruise * 3.6 + 0.5)
-          showHelpNotification("Hastighetsbegränsare inställd på "..cruise.." km/h, tryck ~INPUT_VEH_SUB_ASCEND~ + ~INPUT_MP_TEXT_CHAT_TEAM~ för att inaktivera.")
-        end
-      end
-    else
-      resetSpeedOnEnter = true
-    end
-  end
+		-- This should only happen on vehicle first entry to disable any old values
+		if GetPedInVehicleSeat(vehicle, -1) == playerPed and IsPedInAnyVehicle(playerPed, false) then
+			if resetSpeedOnEnter then
+				maxSpeed = GetVehicleHandlingFloat(vehicle,"CHandlingData","fInitialDriveMaxFlatVel")
+				SetEntityMaxSpeed(vehicle, maxSpeed)
+				resetSpeedOnEnter = false
+			end
+			-- Disable speed limiter
+			if IsControlJustReleased(0,246) and IsControlPressed(0,131) then
+				maxSpeed = GetVehicleHandlingFloat(vehicle,"CHandlingData","fInitialDriveMaxFlatVel")
+				SetEntityMaxSpeed(vehicle, maxSpeed)
+				showHelpNotification("Hastighetsbegränsare inaktiverad")
+			-- Enable speed limiter
+			elseif IsControlJustReleased(0,246) then
+				cruise = GetEntitySpeed(vehicle)
+				SetEntityMaxSpeed(vehicle, cruise)
+				cruise = math.floor(cruise * 3.6 + 0.5)
+				showHelpNotification("Hastighetsbegränsare inställd på "..cruise.." km/h, tryck ~INPUT_VEH_SUB_ASCEND~ + ~INPUT_MP_TEXT_CHAT_TEAM~ för att inaktivera.")
+			end
+		else
+			resetSpeedOnEnter = true
+		end
+	end
 end)
 
 function showHelpNotification(msg)
-    BeginTextCommandDisplayHelp("STRING")
-    AddTextComponentSubstringPlayerName(msg)
-    EndTextCommandDisplayHelp(0, 0, 1, -1)
+	BeginTextCommandDisplayHelp("STRING")
+	AddTextComponentSubstringPlayerName(msg)
+	EndTextCommandDisplayHelp(0, 0, 1, -1)
 end
