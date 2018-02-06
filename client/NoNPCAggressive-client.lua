5236e6946dca7850ed8d135f87ce48dff73ba203
ElPumpo <samo_kol@hotmail.com>
2018-02-06 20:49:53 +0100

    Removed broken script

--- client/NoNPCAggressive-client.lua
+++
@@ -1,8 +0,0 @@
--- source https://forum.fivem.net/t/help-stop-npc-to-attack/77864/2?u=hawaii_beach
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-		SetEveryoneIgnorePlayer(PlayerId(), true)
-		SetPlayerCanBeHassledByGangs(PlayerId(), true)
-	end
-end)
