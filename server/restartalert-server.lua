abcd355108448024cab7104f8a2f2c8bc6cf708d
ElPumpo <samo_kol@hotmail.com>
2018-03-17 21:56:55 +0100

    Revert "Translated and added 30 sec message"
    This reverts commit ae65d857f62cc92950b612771f022e16176e9ab2.

--- server/restartalert-server.lua
+++ server/restartalert-server.lua
@@ -8,28 +8,21 @@ end)
 
 AddEventHandler('chatMessage', function(player, playerName, message)
     if message:sub(1) == '/svr 5' then --                        <- Command used to display the message          Message displayed server wide.
-        TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART OM 5 MINUTER \n Kör klart ditt pågående RP scenario och ta dig till ett garage! \n ——————————————————————", {239, 0, 0})
+        TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART IN 5 MINUTES \n Please Finish Your Current RP Senario \n ——————————————————————", {239, 0, 0})
         CancelEvent()
     end
 end)
 
 AddEventHandler('chatMessage', function(player, playerName, message)
     if message:sub(1) == '/svr 10' then --                        <- Command used to display the message          Message displayed server wide.
-        TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART OM 10 MINUTER \n Kör klart ditt pågående RP scenario och ta dig till ett garage! \n ——————————————————————", {239, 0, 0})
+        TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART IN 10 MINUTES \n Please Finish Your Current RP Senario \n ——————————————————————", {239, 0, 0})
         CancelEvent()
     end
 end)
 
 AddEventHandler('chatMessage', function(player, playerName, message)
     if message:sub(1) == '/svr now' then
-        TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART PÅGÅR \n Var snäll att logga ut från servern!  \n ——————————————————————", {239, 0, 0})
-        CancelEvent()
-    end
-end)
-
-AddEventHandler('chatMessage', function(player, playerName, message)
-    if message:sub(1) == '/svr 30' then
-        TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART OM 30 SEKUNDER \n Var snäll att logga ut från servern!  \n ——————————————————————", {239, 0, 0})
+        TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART IS IN PROGRESS \n Please Rejoin the server  \n ——————————————————————", {239, 0, 0})
         CancelEvent()
     end
 end)
