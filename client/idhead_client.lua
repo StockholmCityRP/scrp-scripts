e5c4b9a55b82250887d7abc1c86197e62a712c5b
ElPumpo <samo_kol@hotmail.com>
2018-03-25 13:47:48 +0200

    Improved id head by a lot
    - Performance gains
    - Better code

--- client/idhead_client.lua
+++ client/idhead_client.lua
@@ -1,69 +1,35 @@
--- FiveM Client Script By Dawson G. Altered by Valderg AKA Jesus Utah & Dunko
- 
-local showPlayerBlips = false
-local ignorePlayerNameDistance = false
-local disPlayerNames = 15
-local playerSource = 0
- 
-function DrawText3D(x,y,z, text, r,g,b) -- some useful function, use it if you want!
-    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
-    local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
- 
-    local scale = (1/dist)*2
-    local fov = (1/GetGameplayCamFov())*100
-    local scale = scale*fov
-   
-    if onScreen then
-        SetTextScale(0.0*scale, 0.55*scale)
-        SetTextFont(0)
-        SetTextProportional(1)
-        -- SetTextScale(0.0, 0.55)
-        SetTextColour(r, g, b, 255)
-        SetTextDropshadow(0, 0, 0, 0, 255)
-        SetTextEdge(2, 0, 0, 0, 150)
-        SetTextDropShadow()
-        SetTextOutline()
-        SetTextEntry("STRING")
-        SetTextCentre(1)
-        AddTextComponentString(text)
-        DrawText(_x,_y)
-    end
-end
- 
+-- Modified version from Scammer's id over head + blip
+
 Citizen.CreateThread(function()
-    while true do
-        for i=0,99 do
-            N_0x31698aa80e0223f8(i)
-        end
-        for id = 0, 31 do
-            if  ((NetworkIsPlayerActive( id )) and GetPlayerPed( id ) ~= GetPlayerPed( -1 )) then
-                ped = GetPlayerPed( id )
-                blip = GetBlipFromEntity( ped )
- 
-                x1, y1, z1 = table.unpack( GetEntityCoords( GetPlayerPed( -1 ), true ) )
-                x2, y2, z2 = table.unpack( GetEntityCoords( GetPlayerPed( id ), true ) )
-                distance = math.floor(GetDistanceBetweenCoords(x1,  y1,  z1,  x2,  y2,  z2,  true))
- 
-                if(ignorePlayerNameDistance) then
-                    if NetworkIsPlayerTalking( id ) then
-                        DrawText3D(x2, y2, z2+1, GetPlayerServerId(id), 128,185,255) -- blue color
-					else
-                        DrawText3D(x2, y2, z2+1, GetPlayerServerId(id), 255,255,255)
-					end
-                end
- 
-                if ((distance < disPlayerNames)) then
-                    if not (ignorePlayerNameDistance) then
-					if NetworkIsPlayerTalking( id ) then
-                        DrawText3D(x2, y2, z2+1, GetPlayerServerId(id), 128,185,255) -- blue color
-					else
-                        DrawText3D(x2, y2, z2+1, GetPlayerServerId(id), 255,255,255)
-					end
-                    end
-                end  
-            end
-        end
-        Citizen.Wait(0)
-    end
+	local sourcePed = GetPlayerPed(-1)
+	local targetPed
+	
+	while true do
+	
+		-- For every active player in a 32 slot server
+		for id = 0, 31 do
+			targetPed = GetPlayerPed(id)
+			
+			-- Is the player connected and not the same as the source?
+			if NetworkIsPlayerActive(id) and targetPed ~= GetPlayerPed(-1) then
+				
+				-- Is the source near the target, and is there a clear view? (so that people cannot see IDs through walls etc)
+				if (GetDistanceBetweenCoords(GetEntityCoords(sourcePed), GetEntityCoords(targetPed)) < 15.0001) and HasEntityClearLosToEntity(sourcePed, targetPed, 17) then
+					
+					-- Create head display (this is safe to be spammed)
+					headId = Citizen.InvokeNative(0xBFEFE3321A3F5015, targetPed, ' ' .. GetPlayerServerId(id), false, false, "", false)
+					Citizen.InvokeNative(0x63BB75ABEDC1F6A0, headId, 0, true)
+					
+					-- Speaking display
+					Citizen.InvokeNative(0x63BB75ABEDC1F6A0, headId, 9, NetworkIsPlayerTalking(id))
+				else
+					-- Remove blip as the player is too far away
+					Citizen.InvokeNative(0x63BB75ABEDC1F6A0, headId, 0, false)
+				end
+			end
+		end
+		
+		-- We can wait two seconds without losing anything, you do NOT want to run this every 10 frames like other people have done...
+		Citizen.Wait(2000)
+	end
 end)
