808a9bff6e0e9ea0bbccdff46b440525c503a96a
ElPumpo <samo_kol@hotmail.com>
2018-03-26 19:35:28 +0200

    Attempt #1 to fix id head
    'but it worked just fine on the dev server!'

--- client/idhead_client.lua
+++ client/idhead_client.lua
@@ -1,28 +1,23 @@
 -- Modified version from Scammer's id over head + blip
 
 Citizen.CreateThread(function()
-
-	-- Wait just a bit so that everything can load
-	Citizen.Wait(10000)
 	
-	local sourcePed = GetPlayerPed(-1)
-	local targetPed
-	local headId
+	--Wait just a bit so that everything can load
+	Citizen.Wait(2000)
 	
 	while true do
 	
 		-- For every active player in a 32 slot server
 		for id = 0, 31 do
-			targetPed = GetPlayerPed(id)
 			
 			-- Is the player connected and not the same as the source?
-			if NetworkIsPlayerActive(id) and targetPed ~= GetPlayerPed(-1) and GetPlayerName(id) ~= nil then
+			if NetworkIsPlayerActive(id) and GetPlayerPed(id) ~= GetPlayerPed(-1) and GetPlayerName(id) ~= nil then
 				
 				-- Is the source near the target, and is there a clear view? (so that people cannot see IDs through walls etc)
-				if (GetDistanceBetweenCoords(GetEntityCoords(sourcePed), GetEntityCoords(targetPed)) < 15.0001) and HasEntityClearLosToEntity(sourcePed, targetPed, 17) then
+				if (GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), GetEntityCoords(GetPlayerPed(id))) < 15.0001) and HasEntityClearLosToEntity(GetPlayerPed(-1), GetPlayerPed(id), 17) then
 					
 					-- Create head display (this is safe to be spammed)
-					headId = Citizen.InvokeNative(0xBFEFE3321A3F5015, targetPed, ' ' .. GetPlayerServerId(id), false, false, "", false)
+					headId = Citizen.InvokeNative(0xBFEFE3321A3F5015, GetPlayerPed(id), ' ' .. GetPlayerServerId(id), false, false, "", false)
 					Citizen.InvokeNative(0x63BB75ABEDC1F6A0, headId, 0, true)
 					
 					-- Speaking display
@@ -35,6 +30,6 @@ Citizen.CreateThread(function()
 		end
 		
 		-- We can wait two seconds without losing anything, you do NOT want to run this every 10 frames like other people have done...
-		Citizen.Wait(2000)
+		Citizen.Wait(1000)
 	end
 end)
