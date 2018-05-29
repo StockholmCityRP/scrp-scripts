1c5663b4f81eb38256341be904b69081ad5ce593
ElPumpo <samo_kol@hotmail.com>
2018-05-29 19:57:13 +0200

    Removed unneeded translations

--- locates/sv.lua
+++ locates/sv.lua
@@ -1,20 +1,7 @@
 Locales['sv'] = {
-	-- ooc chat
-	['ooc_help'] = 'skicka ett meddelande som inte kopplat till din karaktär',
-	['ooc_prefix'] = 'OOC | %s',
-	['twt_help'] = 'skicka en tweet',
-	['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
-	['me_help'] = 'skriv en händelse, t.ex \'visar körkort\'',
-	['me_prefix'] = 'jag | %s',
-	['news_help'] = 'meddela en nyhet (missbruka inte)',
-	['news_prefix'] = 'TV4 Stockholm',
-	['ooc_argument_name'] = 'meddelande',
-	['ooc_argument_help'] = 'det meddelandet du vill skicka',
-	['ooc_unknown_command'] = 'det ^3kommandot^0 finns ^1inte^0, försök igen!',
-	
 	-- afk script
 	['afk'] = 'anti-AFK',
-	['afk_kicked_message'] = 'du har blivit kickad pga inaktivitet!',
+	['afk_kicked_message'] = 'du har blivit kickad på grund av inaktivitet!',
 	['afk_warning'] = 'du kommer bli kickad om ~r~%s sekunder~w~ på grund av inaktivitet!',
 	
 	-- gps tools
