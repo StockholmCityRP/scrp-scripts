6cbc604267f8c9be5f9397851ab9ad3deaa5c4ae
ElPumpo <samo_kol@hotmail.com>
2018-07-05 20:52:21 +0200

    Improved tpwaypoint

--- client/gpstools-client.lua
+++ client/gpstools-client.lua
@@ -1,11 +1,19 @@
+ESX = nil
 local isMinimapEnabled = false
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(50)
+	end
+end)
 
 RegisterNetEvent('gpstools:setgps')
 AddEventHandler('gpstools:setgps', function(pos)
 	-- add required decimal or else it wont work
 	pos.x = pos.x + 0.00
 	pos.y = pos.y + 0.00
-	
+
 	SetNewWaypoint(pos.x, pos.y)
 	showHelpNotification(_U('gpstools_setgps_ok'))
 end)
@@ -34,17 +42,31 @@ AddEventHandler('gpstools:tpwaypoint', function()
 	local blip = GetFirstBlipInfoId(8)
 
 	if DoesBlipExist(blip) then
-		--local coord = Citizen.InvokeNative(0xFA7C7F0AADF25D09, blip, Citizen.ResultAsVector())
 		local coord = GetBlipInfoIdCoord(blip)
-		SetEntityCoords(playerPed, coord.x, coord.y, coord.z)
-		ClearPedTasksImmediately(playerPed)
-		SetEntityCoords(playerPed, coord.x, coord.y, coord.z)
-
-		RequestCollisionAtCoord(coord.x, coord.y, coord.z)
-		while not HasCollisionLoadedAroundEntity(playerPed) do
+		local groundFound = false
+		local groundCheckHeights = {0.0, 50.0, 100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0,450.0, 500.0, 550.0, 600.0, 650.0, 700.0, 750.0, 800.0}
+		local coordZ
+		for i, height in ipairs(groundCheckHeights) do
+			RequestCollisionAtCoord(coord.x, coord.y, height)
 			Citizen.Wait(0)
-			RequestCollisionAtCoord(coord.x, coord.y, coord.z)
+
+			SetEntityCoordsNoOffset(playerPed, coord.x, coord.y, height, 0, 0, 1)
+
+			local foundGround, z = GetGroundZFor_3dCoord(coord.x, coord.y, height)
+			if foundGround then
+				coordZ = z + 3
+				groundFound = true
+				break
+			end
 		end
+	
+		if not groundFound then
+			coordZ = 100
+			TriggerEvent('esx:addWeapon', 'GADGET_PARACHUTE', 0)
+		end
+
+		SetEntityCoordsNoOffset(playerPed, coord.x, coord.y, coordZ, 0, 0, 1)
+		
 	else
 		showHelpNotification(_U('gpstools_tp_no_waypoint'))
 	end
